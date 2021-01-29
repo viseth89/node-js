fa01636f06462dda5b790bdc4fe83f76f55b3c14
datalogics-vsen <viseth89@gmail.com>
2021-01-29 13:53:07 -0600

    Added mongoose mock

--- todo-tdd/tests/unit/todo.controller.test.js
+++ todo-tdd/tests/unit/todo.controller.test.js
@@ -1,12 +1,20 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model")
+
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
     it("should have a createTodo function", () => {
         expect(typeof TodoController.createTodo).toBe("function");
     });
+    it('should call TodoModel.create', () => {
+        TodoController.createTodo();
+        expect(TodoModel.create).toBeCalled();
+    })
 });
 describe("TodoController.createTodo", () => {
     it("should have a createTodo function", () => {
         expect(typeof TodoController.createtodo2).toBe("function");
     });
+
 });
