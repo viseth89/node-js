69e968d671029bdd8e1cbc7c060f1ecc74f619b2
datalogics-vsen <viseth89@gmail.com>
2021-01-29 13:59:31 -0600

    added jest config file

--- todo-tdd/controllers/todo.controller.js
+++ todo-tdd/controllers/todo.controller.js
@@ -1,7 +1,10 @@
-exports.createTodo = () => {};
+const TodoModel = require("../model/todo.model")
 
-//exports
+exports.createTodo = () => {
+    TodoModel.create()
+};
 
 exports.createtodo2 = () =>{
     console.log('this is wrong')
 }
+
