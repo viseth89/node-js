c398a47538ef5ce38946c787e93ad0df1113d265
datalogics-vsen <viseth89@gmail.com>
2021-01-29 12:20:03 -0600

    Added second route to test config

--- todo-tdd/app.js
+++ todo-tdd/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/",(req, res) => {
     res.send('Hello World!')
 });
+app.get("/hello",(req, res) => {
+    res.send('Hello to you too World!')
+});
 
 app.listen(3000, () =>{
     console.log('Server is now running at port 3000!')
