8c51baf89128dbbc4de38de5b495b4541b48a866
datalogics-vsen <viseth89@gmail.com>
2021-02-01 10:22:24 -0600

    End of video 15
    Reviewing response code

--- todo-tdd/controllers/todo.controller.js
+++ todo-tdd/controllers/todo.controller.js
@@ -1,11 +1,9 @@
 const TodoModel = require("../model/todo.model")
 
 exports.createTodo = (req, res, next) => {
-    TodoModel.create(req.body)
+    TodoModel.create(req.body);
+    res.status(201).send();
+
     // why does req.body work?
+    // why is it ''.send();" to make the expect 'res._isEndCalled()' work;
 };
-
-exports.createtodo2 = () =>{
-    console.log('this is wrong')
-}
-
--- todo-tdd/tests/unit/todo.controller.test.js
+++ todo-tdd/tests/unit/todo.controller.test.js
@@ -5,23 +5,33 @@ const newTodo = require("../mock-data/new-todo.json")
 
 TodoModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+});
+
 describe("TodoController.createTodo", () => {
+
+    beforeEach(() => {
+        req.body = newTodo;
+    })
+
+
+
     it("should have a createTodo function", () => {
         expect(typeof TodoController.createTodo).toBe("function");
     });
     it('should call TodoModel.create', () => {
-        let req, res, next;
-        req = httpMocks.createRequest();
-        res = httpMocks.createRequest();
-        next = null;
-        req.body = newTodo;
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalledWith(newTodo);
     })
-});
-describe("TodoController.createTodo", () => {
-    it("should have a createTodo function", () => {
-        expect(typeof TodoController.createtodo2).toBe("function");
+    it("should return 201 response code", () =>{
+        TodoController.createTodo(req, res, next);
+        expect(res.statusCode).toBe(201);
+        expect(res._isEndCalled()).toBeTruthy();
     });
-
+    it("Should return json body in response", () => {
+    })
 });
