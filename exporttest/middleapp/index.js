632c8ec923202ebb0a81cc1844a450647d27c0b6
viseth89 <viseth89@gmail.com>
2021-11-30 07:02:11 -0600

    added formidable to project, testing with middleware

--- exporttest/middleapp/index.js
+++ exporttest/middleapp/index.js
@@ -4,8 +4,34 @@ var express = require('express')
 const function1 = require('../middleware/middle1')
 const function2 = require('../middleware/middle2')
 const function3 = require('../middleware/middle3')
+const formidable = require("formidable");
+
+const form = formidable({
+    multiples: true,
+    keepExtensions: true,
+    maxFileSize: settings.maxUploadSize * 1024 * 1024,
+  });
 
 var app = express()
+
+app.get("/",function1, function2, function3,   (req, res) => {
+    console.log('original route')
+    
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+          next(err);
+          return;
+        }
+    })
+
+
+    res.send("hey there get")
+})
+
+app.get("/",function1, function2, function3,   (req, res) => {
+    console.log('original route')
+    res.send("hey there get")
+})
 
 app.get("/",function1, function2, function3,   (req, res) => {
     console.log('original route')
